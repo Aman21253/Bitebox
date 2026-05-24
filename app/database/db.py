f67a76d1339b818e640d2f7db7d3679f9c08cf42
Aman Bansal <amanbansal@Aman-MacBook-Pro.local>
2026-05-24 15:33:01 +0530

    Add PostgreSQL driver

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,6 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -13,3 +13,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
